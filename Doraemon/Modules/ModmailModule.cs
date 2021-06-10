e0f8545b10d6c43d7bb1c74592278ac0312816be
n-Ultima <nicbarclay05@gmail.com>
2021-06-09 19:56:25 -0500

    Created contact command

--- Doraemon/Modules/ModmailModule.cs
+++ Doraemon/Modules/ModmailModule.cs
@@ -12,6 +12,9 @@ using Doraemon.Common.Attributes;
 using Discord.WebSocket;
 using Doraemon.Common.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Doraemon.Common;
+using Doraemon.Common.Utilities;
+using Discord.Net;
 
 namespace Doraemon.Modules
 {
@@ -22,6 +25,7 @@ namespace Doraemon.Modules
     {
         public DoraemonContext _doraemonContext;
         public DiscordSocketClient _client;
+        public DoraemonConfiguration DoraemonConfig { get; private set; } = new();
         public ModmailModule(DoraemonContext doraemonContext, DiscordSocketClient client)
         {
             _doraemonContext = doraemonContext;
@@ -88,6 +92,53 @@ namespace Doraemon.Modules
             await dmChannel.SendMessageAsync(embed: embed);
             _doraemonContext.ModmailTickets.Remove(modmail);
             await _doraemonContext.SaveChangesAsync();
+        }
+        [Command("contact")]
+        [Summary("Creates a modmail thread manually with the user.")]
+        public async Task ContactUserAsync(
+            [Summary("The user to contact.")]
+                SocketGuildUser user,
+            [Summary("The message to be sent to the user upon the ticket being created.")]
+                [Remainder] string message)
+        {
+            var modmail = await _doraemonContext
+                .Set<ModmailTicket>()
+                .Where(x => x.UserId == user.Id)
+                .SingleOrDefaultAsync();
+            if(modmail is not null)
+            {
+                throw new Exception($"There is already an ongoing thread with this user in <#{modmail.ModmailChannel}>.");
+            }
+            var modmailCategory = Context.Guild.GetCategoryChannel(DoraemonConfig.ModmailCategory);
+            var textChannel = await Context.Guild.CreateTextChannelAsync(await user.GetFullUsername(), x => x.CategoryId = modmailCategory.Id);
+            var dmChannel = await user.GetOrCreateDMChannelAsync();
+            var highestRole = (Context.User as SocketGuildUser).Roles.OrderByDescending(x => x.Position).First().Name;
+            var userEmbed = new EmbedBuilder()
+                .WithTitle($"You have been contacted by the Staff of {Context.Guild.Name}")
+                .WithAuthor(await Context.User.GetFullUsername(), Context.User.GetAvatarUrl() ?? Context.User.GetDefaultAvatarUrl())
+                .WithDescription(message)
+                .WithColor(Color.Green)
+                .WithFooter($"{highestRole} • {Context.Message.CreatedAt.ToString("f")}")
+                .Build();
+            try
+            {
+                await dmChannel.SendMessageAsync(embed: userEmbed);
+            }
+            catch(HttpException ex) when (ex.DiscordCode == 50007)
+            {
+                throw new Exception("The user provided has Direct Messages disabled, thus I was unable to contact them.");
+            }
+            var ID = await DatabaseUtilities.ProduceIdAsync();
+            await textChannel.SendMessageAsync($"Thread successfully started with {await user.GetFullUsername()}\nID: `{ID}`\nContacter: {await Context.User.GetFullUsername()}");
+            _doraemonContext.ModmailTickets.Add(new ModmailTicket
+            {
+                DmChannel = dmChannel.Id,
+                ModmailChannel = textChannel.Id,
+                Id = ID,
+                UserId = user.Id
+            });
+            await _doraemonContext.SaveChangesAsync();
+            await Context.AddConfirmationAsync();
         }
     }
 }
