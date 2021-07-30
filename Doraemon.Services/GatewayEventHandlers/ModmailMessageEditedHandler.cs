03edd99801a3f8d9eb849b32beb0d6249515e45c
n-Ultima <nicbarclay05@gmail.com>
2021-07-30 00:08:56 -0500

    ugh

---
+++ Doraemon.Services/GatewayEventHandlers/ModmailMessageEditedHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.CodeDom;
+using System.Linq;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Api;
+using Disqord.Gateway;
+using Disqord.Rest;
+using Disqord.Rest.Api;
+using Doraemon.Common;
+using Doraemon.Services.Core;
+using Doraemon.Services.Moderation;
+using Doraemon.Services.Modmail;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Doraemon.Services.GatewayEventHandlers
+{
+    public class ModmailMessageEditedHandler : DoraemonEventService
+    {
+        private readonly ModmailTicketService _modmailTicketService;
+
+        public DoraemonConfiguration DoraemonConfig { get; private set; } = new();
+        public ModmailMessageEditedHandler(AuthorizationService authorizationService, InfractionService infractionService, ModmailTicketService modmailTicketService)
+            : base(authorizationService, infractionService)
+            => _modmailTicketService = modmailTicketService;
+
+        // We will use this method to handle messages edited in modmail threads only. We pray to lord Duck that the cache does cache things
+        // please work cache
+        protected override async ValueTask OnMessageUpdated(MessageUpdatedEventArgs e)
+        {
+            if (e.NewMessage is not IUserMessage message) return;
+            if (message.Author.IsBot) return;
+            var modmailGuild = Bot.GetGuild(DoraemonConfig.MainGuildId);
+            var modmailThread = await _modmailTicketService.FetchModmailTicketByModmailChannelIdAsync(message.ChannelId);
+            // Came from DM
+            if (e.GuildId == null)
+            {
+                modmailThread = await _modmailTicketService.FetchModmailTicketByDmChannelIdAsync(message.ChannelId);
+                if (modmailThread == null)
+                    return;
+                var localEmbed = new LocalEmbed()
+                    .WithTimestamp(DateTimeOffset.UtcNow)
+                    .WithColor(Color.Gold)
+                    .WithTitle($"Message Edited")
+                    .WithDescription($"**Before:**: {e.OldMessage.Content ?? "Cache failure."}\n**After:** {message.Content ?? "Cache failure"}")
+                    .WithAuthor(message.Author)
+                    .WithFooter($"Edited Message Id: {message.Id}");
+                if (message.Attachments.Any())
+                {
+                    localEmbed.WithImageUrl(message.Attachments[0].Url);
+                }
+
+                var modmailThreadChannel = modmailGuild.GetChannel(modmailThread.ModmailChannelId) as ITextChannel;
+                await modmailThreadChannel.SendMessageAsync(new LocalMessage().WithEmbeds(localEmbed));
+                await _modmailTicketService.AddMessageToModmailTicketAsync(modmailThread.Id, message.Author.Id, $"Message edited by {message.Author.Tag}\nBefore: {e.OldMessage.Content}\nAfter: {e.NewMessage.Content}");
+                
+            }
+            // Edit came from modmail channel
+            else
+            {
+                if (modmailThread == null)
+                    return;
+                var localEmbed = new LocalEmbed()
+                    .WithTimestamp(DateTimeOffset.UtcNow)
+                    .WithColor(Color.Green)
+                    .WithTitle($"Message Edited")
+                    .WithDescription($"**Before:** {e.OldMessage.Content ?? "Cache failure"}\n**After:** {e.NewMessage.Content ?? "Cache failure"}")
+                    .WithFooter($"Edited Message Id: {message.Id}");
+                if (message.Attachments.Any())
+                {
+                    localEmbed.WithImageUrl(message.Attachments[0].Url);
+                }
+
+                await _modmailTicketService.AddMessageToModmailTicketAsync(modmailThread.Id, message.Author.Id, $"Message edited by {message.Author.Tag}\nBefore: {e.OldMessage.Content}\nAfter: {e.NewMessage.Content}");
+
+                await Bot.SendMessageAsync(modmailThread.DmChannelId, new LocalMessage().WithEmbeds(localEmbed));
+            }
+        }
+    }
+}
---
+++ Doraemon.Services/GatewayEventHandlers/ModmailThreadMessageReceived.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot.Hosting;
+using Disqord.Gateway;
+using Disqord.Rest;
+using Doraemon.Common;
+using Doraemon.Common.Extensions;
+using Doraemon.Data.Models.Core;
+using Doraemon.Services.Core;
+using Doraemon.Services.Moderation;
+using Doraemon.Services.Modmail;
+
+namespace Doraemon.Services.GatewayEventHandlers
+{
+    public class ModmailThreadMessageReceivedHandler : DoraemonEventService
+    {
+        private readonly ModmailTicketService _modmailTicketService;
+        private readonly GuildUserService _guildUserService;
+        public DoraemonConfiguration DoraemonConfig { get; private set; } = new();
+        
+        public ModmailThreadMessageReceivedHandler(AuthorizationService authorizationService, InfractionService infractionService, ModmailTicketService modmailTicketService, GuildUserService guildUserService)
+            : base(authorizationService, infractionService)
+        {
+            _modmailTicketService = modmailTicketService;
+            _guildUserService = guildUserService;
+        }
+
+        protected override async ValueTask OnMessageReceived(BotMessageReceivedEventArgs eventArgs)
+        {
+            if (eventArgs.Channel == null) return;
+            if (eventArgs.Message is not IUserMessage message) return;
+            var modmailGuild = Bot.GetGuild(DoraemonConfig.MainGuildId);
+            var modmailCategory = modmailGuild.GetChannel(DoraemonConfig.ModmailCategoryId) as ICategoryChannel;
+            var originatingChannel = await message.FetchChannelAsync() as ITextChannel;
+            var guildUser = message.Author as IMember;
+            var guildUserHighestRole = guildUser.GetRoles()
+                .OrderByDescending(x => x.Value.Position)
+                .Select(x => x.Value.Name)
+                .First();
+            if (originatingChannel.CategoryId.Value != DoraemonConfig.ModmailCategoryId) return;
+            var ongoingModmailThread = await _modmailTicketService.FetchModmailTicketByModmailChannelIdAsync(message.ChannelId);
+            if (ongoingModmailThread == null) return;
+            var dmChannel = ongoingModmailThread.DmChannelId;
+            if (message.Content.StartsWith(DoraemonConfig.Prefix)) return; // Ignore commands
+            var embed = new LocalEmbed()
+                .WithColor(Color.Green)
+                .WithAuthor(message.Author)
+                .WithDescription(message.Content)
+                .WithTimestamp(DateTimeOffset.UtcNow)
+                .WithFooter(guildUserHighestRole);
+            if (message.Attachments.Any())
+            {
+                embed.WithImageUrl(message.Attachments.ElementAt(0).Url);
+            }
+
+            await Bot.SendMessageAsync(dmChannel, new LocalMessage().WithEmbeds(embed));
+            await message.AddConfirmationAsync(originatingChannel as CachedGuildChannel);
+            await _modmailTicketService.AddMessageToModmailTicketAsync(ongoingModmailThread.Id, message.Author.Id, $"(Staff){message.Author.Tag} - {message.Content}\n");
+        }
+    }
+}
