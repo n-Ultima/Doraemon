7ed6e9f90b714096428814ee15deb491bfd75d58
n-Ultima <nicbarclay05@gmail.com>
2021-08-21 16:52:07 -0500

    Update Disqord Version

--- Doraemon/Modules/FunModule.cs
+++ Doraemon/Modules/FunModule.cs
@@ -10,7 +10,9 @@ using Doraemon.Data.Models;
 using Doraemon.Services.Moderation;
 using Humanizer;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Qmmands;
+using Serilog;
 
 namespace Doraemon.Modules
 {
@@ -33,6 +35,29 @@ namespace Doraemon.Modules
             if (deserializedResult.Facts.Count == 0 || !deserializedResult.Success)
                 throw new Exception("There was an error fetching a fact from the API.");
             return Response($"Dogfact: {deserializedResult.Facts[0]}");
+        }
+
+        [Command("meme")]
+        [Description("Fetches a meme from the r/memes subreddit.")]
+        public async Task<DiscordCommandResult> FetchMemeAsync()
+        {
+            var redditUrl = "https://www.reddit.com/r/memes/random.json?limit=1";
+            var jsonMeme = await _httpClient.GetStringAsync(redditUrl);
+            var jsonMemeArray = JArray.Parse(jsonMeme);
+            var redditPost = JObject.Parse(jsonMemeArray[0]["data"]["children"][0]["data"].ToString());
+            Boolean.TryParse(redditPost["over_18"].ToString(), out var isNsfw);
+            if (isNsfw)
+            {
+                Log.Logger.Information($"{Context.Author.Tag} attempted to run the meme command, but an NSFW meme was retrieved.");
+                return Response("The post retrieved was marked NSFW, please run the command again.");
+            }
+            var embed = new LocalEmbed()
+                .WithTitle(redditPost["title"].ToString())
+                .WithImageUrl(redditPost["url"].ToString())
+                .WithUrl($"https://www.reddit.com{redditPost["permalink"]}")
+                .WithColor(DColor.Red)
+                .WithFooter($"⬆️{redditPost["ups"]}");
+            return Response(embed);
         }
 
         [Command("pocketsand")]
