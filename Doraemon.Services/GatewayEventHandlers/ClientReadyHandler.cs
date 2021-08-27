0c0288c075116250f6f9b6e06227e7f0a2cd67da
n-Ultima <nicbarclay05@gmail.com>
2021-08-26 20:59:11 -0500

    Added checks for if the bot is attempted to be ran in an unauthed guild, add UtilityModule

--- Doraemon.Services/GatewayEventHandlers/ClientReadyHandler.cs
+++ Doraemon.Services/GatewayEventHandlers/ClientReadyHandler.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 using Disqord;
 using Disqord.Gateway;
 using Disqord.Rest;
+using Doraemon.Common;
 using Doraemon.Data.Models.Moderation;
 using Doraemon.Services.Core;
 using Doraemon.Services.Moderation;
 using Humanizer;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 
 namespace Doraemon.Services.GatewayEventHandlers
@@ -18,23 +20,30 @@ namespace Doraemon.Services.GatewayEventHandlers
     public class ClientReadyHandler : DoraemonEventService
     {
         private const string muteRoleName = "Doraemon_Moderation_Mute";
-
-        public ClientReadyHandler(AuthorizationService authorizationService, InfractionService infractionService)
+        private readonly IHostApplicationLifetime _applicationLifetime;
+        public DoraemonConfiguration DoraemonConfig { get; private set; } = new();
+        public ClientReadyHandler(AuthorizationService authorizationService, InfractionService infractionService, IHostApplicationLifetime applicationLifetime)
             : base(authorizationService, infractionService)
         {
+            _applicationLifetime = applicationLifetime;
         }
-
         public override int Priority => 25;
 
         protected override async ValueTask OnReady(ReadyEventArgs e)
         {
-            await Bot.SetPresenceAsync(UserStatus.Online, new LocalActivity("with the hammer", ActivityType.Playing));
+            await Bot.SetPresenceAsync(UserStatus.Online, new LocalActivity("ultima.one/discord", ActivityType.Custom));
             if (e.GuildIds.Count != 1)
             {
-                throw new InvalidOperationException($"Doraemon should only be run in one guild.");
+                Log.Logger.Fatal($"This bot was designed for one-guild use, however it was found to be in {e.GuildIds.Count()}. Please remove this bot from all other guilds except one and restart.");
+                _applicationLifetime.StopApplication();
             }
 
             var guildToModifyId = e.GuildIds[0]; // only one guild per instance
+            if (guildToModifyId != DoraemonConfig.MainGuildId)
+            {
+                Log.Logger.Fatal($"The MainGuildId provided in config.json does not match the one guild the bot detected. Please ensure that the guildId provided is valid.");
+                _applicationLifetime.StopApplication();
+            }
             var guild = Bot.GetGuild(guildToModifyId);
             await Bot.Chunker.ChunkAsync(guild);
             Log.Logger.Information($"Successfully cached guild: {guild.Name}");
@@ -69,7 +78,6 @@ namespace Doraemon.Services.GatewayEventHandlers
             {
                 await InfractionService.RemoveInfractionAsync(infractionToRescind.Id, "Infraction rescinded automatically", Bot.CurrentUser.Id);
             }
-
             var humanizedInfractions = infractionsToRescind
                 .Select(x => x.Id)
                 .ToList();
---
+++ Doraemon/Modules/UtilityModule.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
+using Disqord.Gateway;
+using Disqord.Rest;
+using Doraemon.Common.Extensions;
+using Qmmands;
+
+namespace Doraemon.Modules
+{
+    [Name("Utility")]
+    [Description("Provides commands more focused on utility than anything else.")]
+    public class UtilityModule : DoraemonGuildModuleBase
+    {
+        [Command("snowflake")]
+        [Description("Provides information on the snowflake provided.")]
+        public DiscordCommandResult CheckSnoflake(
+            [Description("The snowflake to query for information on.")]
+                Snowflake snowflake)
+        {
+            var dateCreatedAt = snowflake.CreatedAt;
+            var embed = new LocalEmbed()
+                .WithTitle($"❄ Snowflake Information")
+                .AddField("Created At", snowflake.CreatedAt.ToString("f"))
+                .WithColor(DColor.Blue);
+            return Response(embed);
+        }
+
+        [Command("message", "messageinfo")]
+        [Description("Shows information about a message.")]
+        public async Task<DiscordCommandResult> GetMessageInfoAsync(
+            [Description("The ID of the message.")]
+                Snowflake messageId)
+        {
+            IMessage message;
+            message = await Bot.FetchMessageAsync(Context.ChannelId, messageId);
+            if (message == null)
+            {
+                foreach (var channel in Context.Guild.GetChannels().Select(x => x.Value))
+                {
+                    if (channel is not ITextChannel textChannel) return null;
+                    message = await textChannel.FetchMessageAsync(messageId);
+                    if (message == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (message == null)
+                {
+                    throw new Exception($"The message ID provided doesn't exist, or the bot can't find it.");
+                }
+            }
+
+            var messageChannel = await message.FetchChannelAsync();
+            var embed = new LocalEmbed()
+                .WithTitle($"Information for message Id {message.Id}")
+                .AddField("Content", message.Content)
+                .AddField("Channel Sent In", $"{Mention.Channel(messageChannel.Id)}")
+                .AddField("Author", $"{Mention.User(message.Author)}")
+                .WithColor(DColor.Gold);
+            return Response(embed);
+        }
+
+        [Command("jumbo", "jumbofy")]
+        [Description("Jumbofies the given emoji.")]
+        public DiscordCommandResult Jumbofy(
+            [Description("The emoji to jumbofy.")]
+                ICustomEmoji emoji)
+        {
+            var emojiUrl = emoji.GetUrl();
+            if (emojiUrl == null)
+                throw new Exception($"I don't recongnize that emoji.");
+            return Response(emojiUrl);
+        }
+    }
+}
